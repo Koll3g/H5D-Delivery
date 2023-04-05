afbdda22acdb9e08ee1db35507e06fa245b0f239
Angelo Lütolf <angelo.luetolf93@gmail.com>
2023-04-05 22:20:51 +0200

    fixed typo

--- H5D-Delivery.Mgmt.Backend/Product/Exceptions/ProductNameInvalidException.cs
+++ H5D-Delivery.Mgmt.Backend/Product/Exceptions/ProductNameInvalidException.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace H5D_Delivery.Mgmt.Backend.Product.Exceptions
 {
-    public class ProductInvalidException : Exception
+    public class ProductNameInvalidException : Exception
     {
-        public ProductInvalidException(string message) : base(message) { }
+        public ProductNameInvalidException(string message) : base(message) { }
     }
 }
