94af30083986164d27b9a6ca3d3dd7e74cf9d239
Angelo Lütolf <angelo.luetolf93@gmail.com>
2023-04-06 21:07:25 +0200

    stockitem framework

---
+++ H5D-Delivery.Mgmt.Backend/Stock/Domain/StockItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using H5D_Delivery.Mgmt.Backend.Shared;
+
+namespace H5D_Delivery.Mgmt.Backend.Stock.Domain
+{
+    public class StockItem : DbItem
+    {
+        public Product.Domain.Product Product { get; set; }
+        public Guid ProductId { get; set; }
+        public uint Amount { get; set; }
+        public uint StorageLocation { get; set; }
+
+        public StockItem(Guid id, Product.Domain.Product product, uint amount, uint storageLocation) : base(id)
+        {
+            Product = product;
+            ProductId = product.Id;
+            Amount = amount;
+            StorageLocation = storageLocation;
+        }
+    }
+}
---
+++ H5D-Delivery.Mgmt.Backend/Stock/Domain/StockService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H5D_Delivery.Mgmt.Backend.Stock.Domain
+{
+    public class StockService : IStockRepository
+    {
+        public IEnumerable<StockItem> GetAll()
+        {
+
+        }
+
+        public StockItem? Get(Guid id)
+        {
+
+        }
+
+        public void Update(StockItem stockItem)
+        {
+
+        }
+
+        public void Delete(Guid id)
+        {
+
+        }
+
+        public void Create(StockItem stockItem)
+        {
+
+        }
+    }
+}
