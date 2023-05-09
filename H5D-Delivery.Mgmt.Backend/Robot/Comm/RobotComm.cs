429798bd88c6e71ed2bfedd306a9ab7c65b81b47
angeloluetolf <angelo.luetolf93@gmail.com>
2023-05-09 14:55:54 +0200

    init robotcomm

---
+++ H5D-Delivery.Mgmt.Backend/Robot/Comm/RobotComm.cs
@@ -0,0 +1,99 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+namespace H5D_Delivery.Mgmt.Backend.Robot.Comm
+{
+    public class RobotComm
+    {
+        private readonly IMqttClient _mqttClient;
+        private Guid _robotId;
+
+        public event EventHandler<int>? BatteryChargePctReceivedEvent;
+
+        private readonly string _batteryChargePctTopic;
+        private readonly string _statusUpdateRequestTopic;
+
+        public RobotComm(Guid robotId)
+        {
+            var factory = new MqttFactory();
+            _mqttClient = factory.CreateMqttClient();
+            _robotId = robotId;
+
+            //Define all Topics
+            _batteryChargePctTopic = $"Robots/{_robotId}/From/Status/BatteryChargePct";
+            _statusUpdateRequestTopic = $"Robots/{_robotId}/To/Requests/StatusUpdate";
+
+            ConnectAndSubscribe();
+        }
+
+        public async void RequestStatusUpdate()
+        {
+            await PublishAsync(_statusUpdateRequestTopic, "1");
+        }
+
+        private async void ConnectAndSubscribe()
+        {
+            await ConnectAsync("localhost", 1883, "RoboSim");
+
+            SubscribeToAllTopics();
+        }
+
+        private async void SubscribeToAllTopics()
+        {
+            await SubscribeAsync(_batteryChargePctTopic);
+        }
+
+        private async Task ConnectAsync(string brokerHostName, int brokerPort, string clientId)
+        {
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(brokerHostName, brokerPort)
+                .WithClientId(clientId)
+                .Build();
+
+            await _mqttClient.ConnectAsync(options);
+        }
+
+        private Task MqttMessageHandler(MqttApplicationMessageReceivedEventArgs x)
+        {
+            var topic = x.ApplicationMessage.Topic;
+
+            if(topic == _batteryChargePctTopic)
+            {
+                return BatteryChargePctReceivedHandler(x);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task BatteryChargePctReceivedHandler(MqttApplicationMessageReceivedEventArgs x)
+        {
+            return Task.Run(() =>
+            {
+                var payload = x.ApplicationMessage.Payload;
+                var value = ConvertToInt(payload);
+                BatteryChargePctReceivedEvent?.Invoke(this, value);
+            });
+        }
+
+        private int ConvertToInt(byte[] payload)
+        {
+            return BitConverter.ToInt32(payload);
+        }
+
+        private async Task PublishAsync(string topic, string payload)
+        {
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic(topic)
+                .WithPayload(payload)
+                .Build();
+
+            await _mqttClient.PublishAsync(message);
+        }
+
+        private async Task SubscribeAsync(string topic)
+        {
+            await _mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(topic).Build());
+            _mqttClient.ApplicationMessageReceivedAsync += MqttMessageHandler;
+        }
+    }
+}
